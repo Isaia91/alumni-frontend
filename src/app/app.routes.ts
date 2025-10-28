0f3b52c33714d1fc21ceb32fcfa4cb1ce62c7ec6
Isaia91 <isaia.maperi@gmail.com>
2025-10-28 11:19:16 +1100

    ar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,5 @@ export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'alumni' },
   { path: 'alumni', component: AlumniListComponent },
   { path: 'alumni/:id', component: AlumniDetailComponent },
+  { path: 'ar', loadComponent: () => import('./features/alumni/alumni-ar/alumni-ar').then(m => m.AlumniAr) },
 ];
